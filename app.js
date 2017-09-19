72cfd346798055a9a9d2a994b8dfdb94d82fcafc
Jacob Laurenti <jlaurenti@Jacobs-MacBook-Pro.local>
2017-09-19 12:09:33 -0400

    Checkpoint 8 Events 2 Completed

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+    var toDos = [];
+    var addToDoForm = document.getElementById('addToDoForm');
 
-  addToDoForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-
-      // get the text
-      let title = newToDoText.value;
-
-      // create a new li
-      let newLi = document.createElement('li');
-
-      // create a new input for the checkbox
-      let checkbox = document.createElement('input');
-
-      // set the input's type to checkbox
-      checkbox.type = "checkbox";
-
-      // set the title
-      newLi.textContent = title;
-
-      // attach it to the ul
-      toDoList.appendChild(newLi);
-
-      // attach the checkbox to the li
-      newLi.appendChild(checkbox);
-
-      //empty the input
+    function createNewToDo() {
+      var newToDoText = document.getElementById('newToDoText');
+      toDos.push({
+        title: newToDoText.value,
+        complete: false
+      });
       newToDoText.value = '';
-    });
-  }
 
+      renderTheUI(todos);
+    }
+
+    function renderTheUI(todos) {
+      var toDoList = document.getElementById('toDoList');
+
+      toDoList.innerHTML = '';
+
+      toDos.forEach(function(toDo) {
+        var newLi = document.createElement('li');
+        var checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+
+        newLi.innerHTML = toDo.title;
+        toDoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+      });
+    }
+
+    addToDoForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      createNewToDo();
+    });
+
+
+    renderTheUI(todos);
+
+  }
 
   window.onload = function() {
      onReady();
